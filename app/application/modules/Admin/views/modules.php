5d3a2e700b61de406fb5dd7da9414601252157eb
Naveed <naveed07786@gmail.com>
2020-03-16 16:37:32 +0500

    psuh

--- app/application/modules/Admin/views/modules.php
+++ app/application/modules/Admin/views/modules.php
@@ -19,9 +19,9 @@
         </div>
                         <div class="zebra" style="width:100%;font-size: 15px;">
            Hotels          <span class="pull-right">
-                        <a href="https://www.phptravels.net/admin/hotels/settings/">
-            <button class="btn btn-danger btn-sm pull-left" style="height:22px;line-height: 12px;margin-right: 5px;"><i class="fa fa-cog"></i> Settings</button>
-            </a>
+                        
+            <button class="btn btn-danger btn-sm pull-left" data-toggle="modal" data-target="#myModal" style="height:22px;line-height: 12px;margin-right: 5px;"><i class="fa fa-cog"></i> Settings</button>
+        
                         <input value="1" data-modulename="Hotels" type="number" id="order_set" class="input-sm form-control pull-left Hotels" style="width:60px;height:22px;margin-right: 5px;">
             <label class="control control--checkbox ellipsis pull-right">
               <input type="checkbox" id="checkedbox" name="" value="1" data-value="Hotels" data-item="hotels">
@@ -31,9 +31,9 @@
         </div>
                 <div class="zebra" style="width:100%;font-size: 15px;">
            Travelhope Hotels          <span class="pull-right">
-                        <a href="https://www.phptravels.net/admin/thhotels/settings/">
+                        
             <button class="btn btn-danger btn-sm pull-left" style="height:22px;line-height: 12px;margin-right: 5px;"><i class="fa fa-cog"></i> Settings</button>
-            </a>
+        
                         <input value="2" data-modulename="TravelhopeHotels" type="number" id="order_set" class="input-sm form-control pull-left TravelhopeHotels" style="width:60px;height:22px;margin-right: 5px;">
             <label class="control control--checkbox ellipsis pull-right">
               <input type="checkbox" id="checkedbox" name="" value="1" checked="" data-value="TravelhopeHotels" data-item="hotels">
@@ -43,9 +43,9 @@
         </div>
                 <div class="zebra" style="width:100%;font-size: 15px;">
            Hotels Combined          <span class="pull-right">
-                        <a href="https://www.phptravels.net/admin/hotelscombined/settings/">
+                        
             <button class="btn btn-danger btn-sm pull-left" style="height:22px;line-height: 12px;margin-right: 5px;"><i class="fa fa-cog"></i> Settings</button>
-            </a>
+        
                         <input value="3" data-modulename="Hotelscombined" type="number" id="order_set" class="input-sm form-control pull-left Hotelscombined" style="width:60px;height:22px;margin-right: 5px;">
             <label class="control control--checkbox ellipsis pull-right">
               <input type="checkbox" id="checkedbox" name="" value="1" data-value="Hotelscombined" data-item="hotels">
@@ -55,9 +55,9 @@
         </div>
                 <div class="zebra" style="width:100%;font-size: 15px;">
            Travelport hotels          <span class="pull-right">
-                        <a href="https://www.phptravels.net/admin/travelport_hotel/settings/">
+                        
             <button class="btn btn-danger btn-sm pull-left" style="height:22px;line-height: 12px;margin-right: 5px;"><i class="fa fa-cog"></i> Settings</button>
-            </a>
+      
                         <input value="4" data-modulename="Travelport_hotels" type="number" id="order_set" class="input-sm form-control pull-left Travelport_hotels" style="width:60px;height:22px;margin-right: 5px;">
             <label class="control control--checkbox ellipsis pull-right">
               <input type="checkbox" id="checkedbox" name="" value="1" data-value="Travelport_hotels" data-item="hotels">
@@ -67,9 +67,9 @@
         </div>
                 <div class="zebra" style="width:100%;font-size: 15px;">
            Hotelbeds          <span class="pull-right">
-                        <a href="https://www.phptravels.net/admin/hotelbeds/settings/">
-            <button class="btn btn-danger btn-sm pull-left" style="height:22px;line-height: 12px;margin-right: 5px;"><i class="fa fa-cog"></i> Settings</button>
-            </a>
+            
+            <button class="btn btn-danger btn-sm pull-left"  style="height:22px;line-height: 12px;margin-right: 5px;"><i class="fa fa-cog"></i> Settings</button>
+
                         <input value="5" data-modulename="Hotelbeds" type="number" id="order_set" class="input-sm form-control pull-left Hotelbeds" style="width:60px;height:22px;margin-right: 5px;">
             <label class="control control--checkbox ellipsis pull-right">
               <input type="checkbox" id="checkedbox" name="" value="1" data-value="Hotelbeds" data-item="hotels">
@@ -79,9 +79,9 @@
         </div>
                 <div class="zebra" style="width:100%;font-size: 15px;">
            TP Hotels          <span class="pull-right">
-                        <a href="https://www.phptravels.net/admin/travelpayoutshotels/settings/">
+                        
             <button class="btn btn-danger btn-sm pull-left" style="height:22px;line-height: 12px;margin-right: 5px;"><i class="fa fa-cog"></i> Settings</button>
-            </a>
+          
                         <input value="6" data-modulename="Travelpayoutshotels" type="number" id="order_set" class="input-sm form-control pull-left Travelpayoutshotels" style="width:60px;height:22px;margin-right: 5px;">
             <label class="control control--checkbox ellipsis pull-right">
               <input type="checkbox" id="checkedbox" name="" value="1" data-value="Travelpayoutshotels" data-item="hotels">
@@ -91,9 +91,9 @@
         </div>
                 <div class="zebra" style="width:100%;font-size: 15px;">
            Expedia          <span class="pull-right">
-                        <a href="https://www.phptravels.net/admin/ean/settings/">
+                       
             <button class="btn btn-danger btn-sm pull-left" style="height:22px;line-height: 12px;margin-right: 5px;"><i class="fa fa-cog"></i> Settings</button>
-            </a>
+
                         <input value="7" data-modulename="Ean" type="number" id="order_set" class="input-sm form-control pull-left Ean" style="width:60px;height:22px;margin-right: 5px;">
             <label class="control control--checkbox ellipsis pull-right">
               <input type="checkbox" id="checkedbox" name="" value="1" data-value="Ean" data-item="hotels">
@@ -106,3 +106,77 @@
   </div>
   <span style="position: absolute; top: 14px; font-size: 17px; color: #fff; right: 32px; font-weight: bold; background: rgba(238, 238, 238, 0.22); padding: 3px; border-radius: 48px; width: 30px; text-align: center;">7</span>
 </div>
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Settings</h4>
+        </div>
+        <div class="modal-body">
+        <div class="row">
+          <div class="col-md-6" style="margin-bottom:10px">
+          <input type="text" name="" class="form-control" id="">
+          </div>
+          </div>
+          <div class="row">
+          <div class="col-md-6" style="margin-bottom:10px">
+          <input type="text" name="" class="form-control" id="">
+          </div>
+          </div>
+          <div class="row">
+          <div class="col-md-6" style="margin-bottom:10px">
+          <input type="text" name="" class="form-control" id="">
+          </div>
+          </div>
+          <div class="row">
+          <div class="col-md-6" style="margin-bottom:10px">
+          <input type="text" name="" class="form-control" id="">
+          </div>
+          </div>
+          <div class="row">
+          <div class="col-md-6" style="margin-bottom:10px">
+          <input type="text" name="" class="form-control" id="">
+          </div>
+          </div>
+          <div class="row">
+          <div class="col-md-6" style="margin-bottom:10px">
+          <input type="text" name="" class="form-control" id="">
+          </div>
+          </div>
+          <div class="row">
+          <div class="col-md-6" style="margin-bottom:10px">
+          <input type="text" name="" class="form-control" id="">
+          </div>
+          </div>
+          <div class="row">
+          <div class="col-md-6" style="margin-bottom:10px">
+          <input type="text" name="" class="form-control" id="">
+          </div>
+          </div>
+          <div class="row">
+          <div class="col-md-6" style="margin-bottom:10px">
+          <input type="text" name="" class="form-control" id="">
+          </div>
+          </div>
+          <div class="row">
+          <div class="col-md-6" style="margin-bottom:10px">
+          <input type="text" name="" class="form-control" id="">
+          </div>
+          </div>
+          
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+
