a337207f45a897684cd2da6b83d39c91f3599f81
Naveed <naveed07786@gmail.com>
2020-03-19 20:59:22 +0500

    push

--- app/themes/default/views/modules/flights/voucher.php
+++ app/themes/default/views/modules/flights/voucher.php
@@ -3,9 +3,19 @@
     <head>
         <meta charset="UTF-8" />
         <title></title>
+        <link rel="stylesheet" href="<?php echo $theme_url;?>assets/css/vocher.css">
+    <style>
+    @media screen and (max-width: 768px){
+      .w-100{
+         width: 100% !important
+}
+.w-25{width:25% !important;}
+}
+
+    </style>
     </head>
     <body style="background-color: rgb(82, 86, 89);font-family: sans-serif;font-size: 15px">
-        <div style="width: 805px; margin:50px auto; background-color:#fff;padding: 15px;border-radius:3px;box-shadow: 5px 5px 8px rgba(0, 0, 0, 0.28);">
+        <div  style="width: 805px; margin:50px auto; background-color:#fff;padding: 15px;border-radius:3px;box-shadow: 5px 5px 8px rgba(0, 0, 0, 0.28);" class="w-100">
             <div style="border-bottom:2px solid #000;margin-bottom: 10px">
                 <h4> 07 FEB 2020 <span style="margin-right: 10px">&#10095;</span>07 FEB 2020 <small style="font-weight: normal;margin-right: 10px;margin-left: 15px" >Trip to</small >NEJRAN, SAUDI ARABIA </h4>
             </div>
@@ -24,7 +34,7 @@
             </table>
             <div> <span style="font-size: 35px;transform: rotate(-45deg); display: inline-block;" >&#9992;</span > DEPARTURE: <strong>FRIDAY 07 FEB</strong> <small style="color: #ccc" >Please verify flight times prior to departure</small > </div>
             <table style="width: 100%;">
-                <td style="background-color:#eee;padding: 10px;position: relative;width: 238px" > <span style="font-size: 18px">SAUDIA AIRLINES</span> <br /> <strong style="font-size: 18px">SV 1840</strong> <small style="display: block;margin: 10px 0 5px 0;font-size: 14px" >Duration:</small > <small style="display: block;margin-bottom: 10px;font-size: 14px" >1hr(s) 45min(s)</small > <small style="font-size: 14px;margin-bottom: 5px;display: block;" >Cabin:</small > <small style="display: block;margin-bottom: 10px;font-size: 14px" >Economy</small > <small style="font-size: 14px;margin-bottom: 5px;display: block;" >Status:</small > <small>Confirmed</small> <br /> <br /> <span style="display: inline-block; position: absolute; border-width: 25px; border-radius: .375rem 0 0 0; width: 0; height: 0; z-index: 6000; border-style: solid; border-top-color: #fff; border-left-color: #fff; border-color: #fff transparent transparent transparent; transform: rotate(45deg); bottom: -35px;left: -28px;" > </span> </td>
+                <td class="w-25" style="background-color:#eee;padding: 10px;position: relative;width: 238px" > <span style="font-size: 18px">SAUDIA AIRLINES</span> <br /> <strong style="font-size: 18px">SV 1840</strong> <small style="display: block;margin: 10px 0 5px 0;font-size: 14px" >Duration:</small > <small style="display: block;margin-bottom: 10px;font-size: 14px" >1hr(s) 45min(s)</small > <small style="font-size: 14px;margin-bottom: 5px;display: block;" >Cabin:</small > <small style="display: block;margin-bottom: 10px;font-size: 14px" >Economy</small > <small style="font-size: 14px;margin-bottom: 5px;display: block;" >Status:</small > <small>Confirmed</small> <br /> <br /> <span style="display: inline-block; position: absolute; border-width: 25px; border-radius: .375rem 0 0 0; width: 0; height: 0; z-index: 6000; border-style: solid; border-top-color: #fff; border-left-color: #fff; border-color: #fff transparent transparent transparent; transform: rotate(45deg); bottom: -35px;left: -28px;" > </span> </td>
                 <td style="vertical-align: top">
                     <table style="border: 2px solid #ddd; width: 100%;">
                         <tr style=" width: 100%;display:inline-block;border-right:1px dotted #ddd;border-bottom: 1px dotted #ddd;padding: 10px" >
@@ -33,8 +43,8 @@
                             <th style="text-align: right;width: 33.3333%"> <small>Aircraft:</small> <br /> <span style="font-size: 13px;">AIRBUS INDUSTRIE</span> <br /> <small>A320 JET</small> </th>
                         </tr>
                         <tr>
-                            <td style="padding: 10px;border-right: 1px dotted #ddd;float: left;width: 33.3333%;padding-bottom: 58px;" > <small>Departing At:</small> <strong style="display: block;margin-bottom: 10px;font-size: 20px;font-weight: 400;" >06:45</strong > <small>Terminal:</small> <br /> <small>NotAvailable</small> </td>
-                            <td style="padding: 10px;border-right: 1px dotted #ddd;float: left;width: 33.3333%;padding-bottom: 58px" > <small>Departing At:</small> <strong style="display: block;margin-bottom: 10px;font-size: 20px;font-weight: 400;" >06:45</strong > <small>Terminal:</small> <br /> <small>NotAvailable</small> </td>
+                            <td style="padding: 10px;border-right: 1px dotted #ddd;float: left;width: 33.3333%;padding-bottom: 58px;" > <small>Departing At:</small> <strong style="display: block;margin-bottom: 10px;font-size: 20px;font-weight: 400;" >06:45</strong > <small>Terminal:</small> <br /> <small style="word-break: break-all;">NotAvailable</small> </td>
+                            <td style="padding: 10px;border-right: 1px dotted #ddd;float: left;width: 33.3333%;padding-bottom: 58px" > <small>Departing At:</small> <strong style="display: block;margin-bottom: 10px;font-size: 20px;font-weight: 400;" >06:45</strong > <small>Terminal:</small> <br /> <small style="word-break: break-all;">NotAvailable</small> </td>
                             <td style="padding: 10px;float: right;margin-top:-64px;width: 33.3333%"> <br><br><br><br /> <small>Distance (in Miles):433</small> <br /><br /> <small>Stop(s):0</small> <br /><br /> <small >Meals: <br /> Meals</small > </td>
                         </tr>
                     </table>
@@ -51,7 +61,7 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td style="font-weight: 100;font-size: 12px;padding-bottom: 15px;"> » ALBAKRI/ABDULLAH MR </td>
+                        <td style="font-weight: 100;font-size: 12px;padding-bottom: 15px;"> ï¿½ ALBAKRI/ABDULLAH MR </td>
                         <td style="font-weight: 100;font-size: 12px;padding-bottom: 15px;"> Check-In Required </td>
                         <td style="font-weight: 100;font-size: 12px;padding-bottom: 15px;"> 0656367060956 </td>
                     </tr>
@@ -60,7 +70,7 @@
             <h3><span style="font-weight: 200;">OTHER:</span>WEDNESDAY 05 AUG</h3>
             <table style="width: 100%; margin-bottom: 10px;">
                 <tr>
-                    <td style="background-color:#eee;padding: 10px;position: relative;width: 238px" > <span style="display:block;margin-bottom: 15px;">OTHER</span> <small> Status: <br /> Confirmed </small> <br /> <br /> <span style="display: inline-block; position: absolute; border-width: 25px; border-radius: .375rem 0 0 0; width: 0; height: 0; z-index: 6000; border-style: solid; border-top-color: #fff; border-left-color: #fff; border-color: #fff transparent transparent transparent; transform: rotate(45deg); bottom: -35px;left: -28px;" > </span> </td>
+                    <td class="w-25" style="background-color:#eee;padding: 10px;position: relative;width: 238px" > <span style="display:block;margin-bottom: 15px;">OTHER</span> <small> Status: <br /> Confirmed </small> <br /> <br /> <span style="display: inline-block; position: absolute; border-width: 25px; border-radius: .375rem 0 0 0; width: 0; height: 0; z-index: 1; border-style: solid; border-top-color: #fff; border-left-color: #fff; border-color: #fff transparent transparent transparent; transform: rotate(45deg); bottom: -35px;left: -28px;" > </span> </td>
                     <td style="vertical-align: top;">
                         <table style="border:2px solid #ddd;width: 100%;">
                             <tr>
@@ -80,6 +90,6 @@
     </body>
 </html>
 
-<div style="width: 805px; margin:-30px auto 25px ; background-color:#fff;padding: 10px;border-radius:3px;box-shadow: 5px 5px 8px rgba(0, 0, 0, 0.28);">
+<div class="w-100" style="width: 805px; margin:-30px auto 25px ; background-color:#fff;padding: 10px;border-radius:3px;box-shadow: 5px 5px 8px rgba(0, 0, 0, 0.28);">
 <a href="#" style="width:100%" class="btn prime"> Download PDF</a>
 </div>
