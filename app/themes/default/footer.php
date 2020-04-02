dc80ca68f35d28e7d5dd52542a2c021e02344e96
Qasim Hussain <compoxition@gmail.com>
2020-04-03 03:04:49 +0500

    update design

--- app/themes/default/footer.php
+++ app/themes/default/footer.php
@@ -9,8 +9,8 @@
                 </div>
                 <div class="c9 o1-sm">
                     <div class="section-info mt-50 rtl-align-right">
-                        <h2>Download almatar App &amp; Get the latest offers</h2>
-                        <h3>Download almatar app now for easier hotel &amp; flights bookings. Enjoy the best booking experience.</h3>
+                        <h2>Download App &amp; Get the latest offers</h2>
+                        <h3>Download app now for easier hotel &amp; flights bookings. Enjoy the best booking experience.</h3>
                     </div>
                 </div>
             </div>
@@ -70,7 +70,7 @@
                 <img alt="" width="100px" src="<?php echo base_url(); ?>assets/img/logo.png">
                 </a>
                 <p class="coy_right">
-                    <span>� 2020 b2b.com.</span>
+                    <span>&copy; 2020 Booknow.co</span>
                     <span class="reserved">  All rights reserved.</span>
                 </p>
             </div>
